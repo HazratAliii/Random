622c01d7867f757501e553ff4e820eb928f04979
HazratAli <hazrat.arisaftech@gmail.com>
2023-10-21 21:02:03 +0600

    soft push

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 console.log("Hello world");
 console.log("Tasin");
+
+// initializing git
+// git init
+
+// For adding:
+// git add .
+
+// for commiting:
+// git commit -m "Message"
+
+// for pushing:
+// git push origin branch name
+
+// for pulling
+// git pull origin branch name
+
+// view git  status
+// git status
+
+// switching between branches
+// git checkout branchname
+
+// creat & switch to new branch
+// git checkout -b branch name
